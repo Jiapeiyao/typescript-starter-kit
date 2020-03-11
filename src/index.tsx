0d79112b77514c2640e01fcceb8ee615c602f2b3
Jiapeiyao <charlesyjp@qq.com>
2020-03-12 00:09:59 +0800

    basic settings

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default function App() {
+    const containerStyle: React.CSSProperties = {
+        position: 'absolute',
+        width: '100vw',
+        height: '100vh',
+        minWidth: 960,
+        minHeight: 540,
+        top: 0,
+        left: 0,
+        display: "flex",
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundImage: 'linear-gradient(to right, #310524, violet)',
+    };
+
+    const textStyle: React.CSSProperties = {
+        color: 'white',
+        width: '40%',
+        fontSize: 50,
+        textAlign: 'center',
+        fontFamily: 'Arial'
+    };
+
+    return (
+        <div style={containerStyle}>
+            <div style={textStyle}>Fontend Starter Kit</div>
+        </div>
+    );
+}
---
+++ webpack_config/dev.js
@@ -0,0 +1,103 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, '..', 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        use: [
+          'awesome-typescript-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          },
+          'postcss-loader'
+        ],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: require('html-webpack-template'),
+      inject: false,
+      appMountId: 'app',
+      filename: 'index.html'
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+};
+
+module.exports = config;
