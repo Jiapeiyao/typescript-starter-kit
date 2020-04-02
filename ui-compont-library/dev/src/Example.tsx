6c84b60a079e7f9a73d48b45b7c8dd82ba13b4b6
Jiapeiyao <charlesyjp@qq.com>
2020-04-02 22:35:13 +0800

    调试脚本，demo

--- ui-compont-library/dev/src/Example.tsx
+++ ui-compont-library/dev/src/Example.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Button from '../../dist/Button';
-// import '../../dist/themes/dark/index.less';
 import '../../dist/themes/default/index.less';
 
 export default function Example() {
-    return <Button text={'Hello'}/>;
+    const [text, setText] = React.useState('Hello!');
+    return <Button text={text} onClick={() => { setText(text === 'Hello!' ? 'Bye!' : 'Hello!') }}/>;
 }
