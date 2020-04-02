54f70caf88f4befb09bd623e80f40b2eb9753483
Jiapeiyao <charlesyjp@qq.com>
2020-04-02 22:24:43 +0800

    default theme in single page app

--- single-page-app/src/index.tsx
+++ single-page-app/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 import Hello from './Hello';
-import './themes/dark/index.less';
+import './themes/default/index.less';
 
 ReactDOM.render(
     React.createElement(hot(Hello)),
