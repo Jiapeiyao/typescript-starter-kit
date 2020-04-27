0039bbaf05f43786a6405cf01bd408dfd46fcdb5
Jiapeiyao <charlesyjp@qq.com>
2020-04-28 03:40:21 +0800

    更新tsconfig

--- ui-compont-library/dev/webpack.config.js
+++ ui-compont-library/dev/webpack.config.js
@@ -3,46 +3,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.tsx'
-  ],
+  entry: ['react-hot-loader/patch', './src/index.tsx'],
   output: {
     path: path.resolve(__dirname, '.', 'dist'),
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
   },
   module: {
     rules: [
       {
         test: /\.ts(x)?$/,
-        use: [
-          'awesome-typescript-loader'
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              config: {
-                path: './postcss.config.js'
-              },
-            },
-          },
-        ],
-        exclude: /\.module\.css$/
+        use: ['awesome-typescript-loader'],
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
@@ -52,51 +26,64 @@ const config = {
             loader: 'css-loader',
             options: {
               importLoaders: 1,
-              modules: true
-            }
+            },
           },
           {
             loader: 'postcss-loader',
             options: {
               config: {
-                path: './postcss.config.js'
+                path: './postcss.config.js',
               },
             },
           },
         ],
-        include: /\.module\.css$/
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: './postcss.config.js',
+              },
+            },
+          },
+        ],
+        include: /\.module\.css$/,
       },
       {
         test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
-        ]
+        use: ['style-loader', 'css-loader', 'less-loader'],
       },
       {
         test: /\.png|svg|gif|jpg$/,
         use: 'url-loader',
-      }
-    ]
+      },
+    ],
   },
   resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.js'
-    ],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
+      'react-dom': '@hot-loader/react-dom',
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: require('html-webpack-template'),
       inject: false,
       appMountId: 'app',
-      filename: 'index.html'
-    })
+      filename: 'index.html',
+    }),
   ],
   optimization: {
     runtimeChunk: 'single',
@@ -105,11 +92,11 @@ const config = {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
-  }
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
 
 module.exports = (env, argv) => {
